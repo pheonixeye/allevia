f587b36a1bb6ca9cf93484eb2dd635bf2776eb51
pheonixeye <drkazgm@gmail.com>
2025-10-14 02:35:31 +0300

    v0.0.5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "220730750"
+    serviceWorkerVersion: "4123984155"
   }
 });
